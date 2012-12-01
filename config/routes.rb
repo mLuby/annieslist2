8850ed673d124fe5a2fe89bde102043cd9f72810
Michael Luby <mcluby@gmail.com>
2012-12-01 16:40:14 -0500

    Added Admin
    made annie admin (from db/seeds.rb)
    
    need to make root route users#sign_in but don't have users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Annieslist2::Application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, :path_names => { :sign_up => "register"}
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,6 +57,9 @@ Annieslist2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+
+
+#  should be root :to => 'users#sign_in'
   root :to => 'locations#index'
 
   # See how all your routes lay out with "rake routes"
